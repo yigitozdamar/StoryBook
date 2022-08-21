2a02075bf1e36044f8dbd14a8954bf31caf4c748
yigit <yigitozdamar@hotmail.com>
2022-08-21 14:52:10 +0300

    Initial Commit

---
+++ StoryBook/StoryBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryBookApp.swift
+//  StoryBook
+//
+//  Created by Yigit Ozdamar on 21.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct StoryBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
